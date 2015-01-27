4f32807b692f26ad76305a57c845f40876d140b2
Glenn Maynard <glenn@zewt.org>
2015-01-26 22:32:47 -0600

    Cleanup.

--- src/maya/marching_cubes/marching_cubes.cu
+++ src/maya/marching_cubes/marching_cubes.cu
@@ -141,15 +141,15 @@ void MarchingCubes::compute_surface(MeshGeom &geom, const Skeleton *skel, float 
     // set the size of the grid cells, and the amount of cells per side
     const int gridRes = 16;
 
-    const int deltas[8][3] = {
-        { 0, 0, 0 },
-        { 1, 0, 0 },
-        { 1, 1, 0 },
-        { 0, 1, 0 },
-        { 0, 0, 1 },
-        { 1, 0, 1 },
-        { 1, 1, 1 },
-        { 0, 1, 1 },
+    const Vec3i_cu deltas[8] = {
+       Vec3i_cu(0, 0, 0),
+       Vec3i_cu(1, 0, 0),
+       Vec3i_cu(1, 1, 0),
+       Vec3i_cu(0, 1, 0),
+       Vec3i_cu(0, 0, 1),
+       Vec3i_cu(1, 0, 1),
+       Vec3i_cu(1, 1, 1),
+       Vec3i_cu(0, 1, 1),
     };
 
     // We have a single surface comprised of any number of bones.  Render the bounding box
@@ -197,23 +197,22 @@ void MarchingCubes::compute_surface(MeshGeom &geom, const Skeleton *skel, float 
                     GridCell cell;
                     for(int i = 0; i < 8; i++)
                     {
-                        int pX = x+deltas[i][0];
-                        int pY = y+deltas[i][1];
-                        int pZ = z+deltas[i][2];
+                        Vec3i_cu p(x, y, z);
+                        p = p + deltas[i];
 
                         // (pX,pY,pZ) is the grid position, eg. [0,15].  Multiply by delta to scale to
                         // the axis-aligned bounding box, add pmin to offset to the bounding box, and
                         // multiply by _tr to convert to world space.
-                        Point_cu pWorld = Point_cu((float) pX, (float) pY, (float) pZ)*delta;
+                        Point_cu pWorld = Point_cu((float) p.x, (float) p.y, (float) p.z)*delta;
                         pWorld = pWorld  + obbox._bb.pmin;
                         pWorld = obbox._tr * pWorld;
 
                         cell.p[i] = pWorld;
 
                         int idx =
-                            pX*gridRes*gridRes +
-                            pY*gridRes +
-                            pZ;
+                            p.x*gridRes*gridRes +
+                            p.y*gridRes +
+                            p.z;
 
                         cell.val[i] = isoBuffer[idx];
 
